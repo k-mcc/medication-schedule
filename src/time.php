d0689ca552f31d3815a973dd190b25664eca7081
k-mcc <67031674+k-mcc@users.noreply.github.com>
2021-01-18 18:23:25 -0500

    Update time.php

--- src/time.php
+++ src/time.php
@@ -12,7 +12,6 @@
 <body onload="onOpen()">
 
   <?php
-  // define variables and set to empty values
 
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -68,7 +67,7 @@ for ($x = 0; $x <= $numMeds; $x++) {
     }
     else {
       $color = 'random';
-      // ASSIGN RANDOM COLOR (in js)
+      // assign random color in js
     }
 
     $doseLim = $info[$x]['numDoses'];
@@ -112,11 +111,8 @@ for ($x = 0; $x <= $numMeds; $x++) {
     for ($m = 0; $m < count($hrs); $m++) {
         if ($sortHrs[$k] == $hrs[$m]) {
           if (in_array($hrIds[$m], $usedNums)) {
-            //ignore it
           } else {
-          //if ($foundHrs == 0) {
             $sortHrIds[] = $hrIds[$m];
-            //$foundHrs = $foundHrs + 1;
             $usedNums[] = $hrIds[$m];
           }
         }
@@ -150,8 +146,6 @@ function onOpen() {
   var minStart = 10;
   var startDay = "<?php echo $firstHour; ?>";
   if (startDay > minStart) startDay = minStart;
-
-  //var lastHr = 23;
 
   for (var h = sortHrs.length - 1; h >= 0; h--) {
     var i = hrIds.indexOf(sortHrIds[h]);
@@ -269,7 +263,6 @@ function onOpen() {
       if (h > 0) {
         var timeDif = hr - parseInt(sortHrs[h-1].toString(), 10);
         if (timeDif > 1) {
-          //add in hour and hour:30 labels
           for (var k = 1; k < timeDif; k++) {
 
             var hour = hr - k;
@@ -330,168 +323,9 @@ function onOpen() {
         }
       }
     }
-
-    /*if (h == 0) {
-      var timeDif = hr - startDay;
-      if (timeDif > 0) {
-        //add in hour and hour:30 labels
-        for (var k = 1; k <= timeDif; k++) {
-
-          var hour = hr - k;
-
-          if (hour == startDay && hour < 12) halfOfDay = "<b>a.m.</b>";
-          else if (hour == 12 || (hour > 12 && hour == startDay)) halfOfDay = "<b>p.m.</b>";
-          else halfOfDay = ":00";
-
-          var clock = hour; // g = hour of dose in 12-hour clock format
-          if (hour == 0) clock = 12;
-          else if (hour > 12) clock = hour - 12;
-
-          var row = table.insertRow(0);
-          row.id = hour.toString();
-          var th = document.createElement('th');
-          th.innerHTML = "" + clock.toString() + halfOfDay;
-          th.style.padding = "0.5rem 1rem";
-          row.appendChild(th);
-
-          var row1 = table.insertRow(1);
-          var th1 = document.createElement('th');
-          th1.style.padding = "0.5rem 1rem";
-          th1.innerHTML = "" + clock.toString() + ":30";
-          row1.appendChild(th1);
-        }
-      }
-    }*/
-
-   }
-
   }
+ }
 }
-
-  //basic med exploration
-
-  //document.getElementById('littleP').innerHTML = '' + passedArray.toString();
-  //var med1 = </?php echo '["' . implode('", "', $info[0]) . '"]' ?>;
-  //var med2 = </?php echo '["' . implode('", "', $info[1]) . '"]' ?>;
-  //var med3 = </?php echo '["' . implode('", "', $info[2]) . '"]' ?>;
-  //var med4 = </?php echo '["' . implode('", "', $info[2]) . '"]' ?>;
-  /*var meds = new Array();
-  meds.push(</?php echo '["' . implode('", "', $info[0]) . '"]' ?>);
-  meds.push(</?php echo '["' . implode('", "', $info[1]) . '"]' ?>);
-
-  for (var i in meds) {
-    var med = meds[i];
-    var numDoses = med[0];
-    var name = med[1];
-    var dose = med[2];
-    var units = med[3];
-    var hr = med[4]; //from this point on, it's going to just be specifying dose times.
-
-    var min = parseInt((med[5]).toString(), 10);
-    if (min < 10) min = ':0' + min;
-    else min = ':' + min;
-
-    document.write('<br /><br />name: ' + name + '<br />dose: ' + dose + ' ' + units + '<br />time: ' + hr + min);
-  }
-
-  for (var i in meds) {
-    var med = meds[i];
-    var numDoses = med[0];
-    var name = med[1];
-    var dose = med[2];
-    var units = med[3];
-    var hr = med[4]; //from this point on, it's going to just be specifying dose times.
-
-    var min = parseInt((med[5]).toString(), 10);
-    if (min < 10) min = ':0' + min;
-    else min = ':' + min;
-
-    document.write('<br /><br />name: ' + name + '<br />dose: ' + dose + ' ' + units + '<br />time: ' + hr + min);
-  }*/
-
-
-
-
-
-
-/*  function onOpen() {
-
-    var table = document.getElementById("medSched");
-    //var hrs = "</?php echo $hrs; ?>";
-    var hrs = "</?php echo json_encode($hrs); ?>";
-    for(var i=0; i<hrs.length; i++){
-        alert(hrs[i]);
-    }
-    //var hrIds = "</?php echo $hrIds; ?>";
-    //var sortHrs = "</?php echo $sortHrs; ?>";
-    //var sortHrIds = "</?php echo $sortHrIds; ?>";
-    //var eventTitles = "</?php echo $eventTitles; ?>";
-    //var mins = "</?php echo $mins; ?>";
-    //var colors = "</?php echo $colors; ?>";
-    //var colSpans = "</?php echo $colSpans; ?>";
-
-    // first hour label on schedule
-    //var startDay = "</?php echo $firstHour; ?>";
-    //if (startDay > 8) startDay = 8;
-    //var lastHr = 23;
-
-    //for (var i = 0; i < sortHrIds.length; i++) {
-
-      //var index = hrIds.indexOf(sortHrIds[i]);
-      var index = 1;
-
-      //var color = colors[index];
-      //var eventTitle = eventTitles[index];
-      //var min = mins[index];
-      //var colSpan = colSpans[index];
-
-      /*var halfOfDay = "";
-      var h = 6;
-      //var h = parseInt(sortHrs[i].toString(), 10); // h = hour of dose in 24-hour clock format
-      if (h == startDay && h < 12) halfOfDay = "<b>a.m.</b>";
-      else if (h == 12 || (h > 12 && h == startDay)) halfOfDay = "<b>p.m.</b>";
-      else halfOfDay = ":00";
-
-      var g = h; // g = hour of dose in 12-hour clock format
-      if (h == 0) {
-        g = 12;
-      } else if (h > 12) {
-        g = h - 12;
-      }
-
-      var row = table.insertRow(0);
-      var th = document.createElement('th');
-      th.innerHTML = "" + 'hi';
-      //th.innerHTML = "" + g.toString() + halfOfDay;
-      th.style.padding = "0.5rem 1rem";
-
-      row.appendChild(th);
-
-      var cell = row.insertCell(1);
-      //cell.id = '' + h.toString() + '-' + i.toString();
-      cell.style.display = "block";
-      cell.innerHTML = 'time would go here  -->' + hrs[0];
-      document.getElementById('littleP').innerHTML = 'little P says the first hour is ' + hrs[0];
-      //cell.innerHTML = "" + eventTitle + "<br />" + 'time would go here'; // title of scheduled dose
-      cell.setAttribute('rowspan', "1"); // 1 by default
-      //cell.setAttribute('class', ("stage-" + color)); // unless color = 'random'
-      cell.setAttribute('class', ("stage-earth"));
-      block.setAttribute('colspan', 4);
-      //block.setAttribute('colspan', colSpan); // width of dose block
-
-      var row = table.insertRow(1);
-      var th2 = document.createElement('th');
-      th.style.padding = "0.5rem 1rem";
-      th.innerHTML = "" + g.toString() + ":30";
-      row.appendChild(th2);
-
-    //}
-}*/
-
-
-
-
-
 </script>
 
 </body>
